b424daca88f196a82e5f5ef855fd6e1eb2dacf72
GetTuh <jakubniemczyk97@gmail.com>
2022-12-24 18:18:08 +0100

    Added example conversion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<div class="window" style="width: auto;margin:.5rem;max-width:40rem">
+		<div class="title-bar">
+			<div class="title-bar-text" style="font-size: medium;">lorem ipsum</div>
+			<div class="title-bar-controls">
+				<button aria-label="Minimize" />
+				<button aria-label="Maximize" />
+				<button aria-label="Close" />
+			</div>
+		</div>
+
+		<fieldset style="margin: 1rem;">
+			<div class="window-body" style="font-size: small;">
+				<p>Lorem ipsum</p>
+			</div>
+		</fieldset>
+		<div class="status-bar">
+			<p
+				class="status-bar-field"
+				style="font-size:large;margin-left:1rem;margin-right:1rem;margin-top:.5rem;margin-bottom: .5rem;padding:1rem"
+			>
+				<center
+					><b
+						>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad pariatur est, ea vitae iste
+						modi mollitia adipisci a. Aliquam quaerat enim incidunt sint nobis accusamus hic rem
+						similique provident voluptate?</b
+					></center
+				>
+			</p>
+		</div>
+		<fieldset style="margin: 1rem;">
+			<div class="window-body" style="font-size: small;">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odit ex minus vero</p>
+				<p>
+					nihil numquam culpa, similique, error ducimus labore praesentium recusandae ipsum, cum
+					eligendi! Odio inventore a hic voluptatum.
+				</p>
+			</div>
+			<textarea
+				readonly
+				style="margin:1rem;font-size: small;height:100%;width:-webkit-fill-available;width:-moz-available;padding:.5rem"
+				>Lorem ipsum dolor sit amet consectetur adipisicing elit.</textarea
+			>
+		</fieldset>
+	</div>
+</div>
---
+++ src/routes/convert.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let demo = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad pariatur est, ea vitae iste
+						modi mollitia adipisci a. Aliquam quaerat enim incidunt sint nobis accusamus hic rem
+						similique provident voluptate`
+
+	let demo2: string[] = [];
+    const betterarr=demo.split('#')
+    
+	let arr = betterarr[0].split('\n');
+
+	for (let i = 0; i < arr.length; i++) {
+		let currentLine = arr[i];
+		const headerRegex = new RegExp('(# +)(.*)');
+		if (headerRegex.test(currentLine)) {
+			//replace # with empty (or h1)
+			currentLine = `<div class="window" style="width:39rem; padding:1rem"><div class="title-bar" style="height:2rem"><h4 class="title-bar-text">${currentLine}</h4><div class="title-bar-controls">
+    <button aria-label="Close"></button>
+  </div></div>`;
+		}
+		demo2.push(currentLine + `</br>`);
+	}</div>
+</script>
+
+<body>
+	{@html demo2}
+</body>
+
+<style>
+</style>
