8086eac05d683dbc7408a7afb14935c6756d8a0a
GetTuh <jakubniemczyk97@gmail.com>
2023-04-03 23:43:49 +0200

    title fetchin

--- src/routes/components/TitleBar.svelte
+++ src/routes/components/TitleBar.svelte
@@ -2,13 +2,25 @@
 	export let text: string;
 </script>
 
-<div class="title-bar">
-	
-	<img src={'https://win98icons.alexmeub.com/icons/png/user_computer-1.png'} alt={'user_computer-1.png'} style="height:1rem" />
-	<div class="title-bar-text" style="font-size: medium;margin-right: 23rem;">{text}</div>
+<div class="title-bar" style="display:flex;">
+	<img
+		src={'https://win98icons.alexmeub.com/icons/png/user_computer-1.png'}
+		alt={'user_computer-1.png'}
+		style="height:1rem"
+	/>
+	<div class="title-bar-text">{text}</div>
 	<div class="title-bar-controls">
 		<button aria-label="Minimize" />
 		<button aria-label="Maximize" />
 		<button aria-label="Close" />
 	</div>
 </div>
+
+<style>
+	.title-bar-text {
+		margin-left: 0.3rem;
+		font-size: medium;
+		align-self: flex-start;
+		margin-right: auto;
+	}
+</style>
